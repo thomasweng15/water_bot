0c37d1ed334dba054334426c6bae2d6e80928ce1
thomasweng15 <thomas.weng11@gmail.com>
2016-08-25 08:02:48 +0000

    Added send and receive phone numbers and successfully sent text

--- waterbot.py
+++ waterbot.py
@@ -10,12 +10,13 @@ OUTPUT_PIN = 11
 class WaterBot:
 	def __init__(self, output_pin):
 		self.config = self._get_config();
+		self.private_config = self._get_private_config();
 		self.tw_client = self._init_twilio()		
 		self._init_gpio(output_pin)
 
 	def water(self):
 		self._gpio_out()
-		# self._send_text()
+		self._send_text()
 		
 	def _gpio_out(self, t=5):
 		GPIO.output(OUTPUT_PIN, True)
@@ -25,26 +26,28 @@ class WaterBot:
 
 	def _send_text(self):
 		message = self.tw_client.messages.create(
-			to=self.config["receive_number"],
-			from_=self.config["send_number"],
+			to=self.private_config["receive_number"],
+			from_=self.private_config["send_number"],
 			body=self.config["msg_body"]
 		)
 
 		print self.config["msg_body"]
 
 	def _init_twilio(self):
-		self.auth_token = self._get_token();
-		return TwilioRestClient(self.config["account_sid"], self.auth_token)
+		return TwilioRestClient(
+			self.config["account_sid"], 
+			self.private_config["auth_token"]
+		)
 
 	def _get_config(self):
 		with open("config.json", "r") as f:
 			config = f.read().strip()
 		return json.loads(config)
 
-	def _get_token(self):
-		with open("auth_token.txt", "r") as f:
-			token = f.read().strip()
-		return token	
+	def _get_private_config(self):
+		with open("private_config.json", "r") as f:
+			private = f.read().strip()
+		return json.loads(private)	
 
 	def _init_gpio(self, output_pin):
 		GPIO.setmode(GPIO.BCM)
