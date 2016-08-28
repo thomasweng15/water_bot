333b0bcad3d92a66f3d02deceabfa9235ce76585
thomasweng15 <thomas.weng11@gmail.com>
2016-08-28 08:22:41 +0000

    Added arguments for config paths, datetime logging, and better gpio cleanup

--- waterbot.py
+++ waterbot.py
@@ -1,28 +1,34 @@
 # Must run as root on rpi
 
 from twilio.rest import TwilioRestClient
+from datetime import datetime
 import RPi.GPIO as GPIO
 import time
 import json
+import sys
 
-OUTPUT_PIN = 11
+OUTPUT_PIN = 17
+CONFIG = "config.json"
+PRIVATE_CONFIG = "private_config.json"
 
 class WaterBot:
-	def __init__(self, output_pin):
-		self.config = self._get_config();
-		self.private_config = self._get_private_config();
+	def __init__(self, output_pin, config, private_config):
+		self.config = self._get_config(config);
+		self.private_config = self._get_private_config(private_config);
 		self.tw_client = self._init_twilio()		
 		self._init_gpio(output_pin)
 
 	def water(self):
 		self._gpio_out()
-		self._send_text()
+ 		self._send_text()
 		
-	def _gpio_out(self, t=5):
+	def cleanup(self):
+		GPIO.cleanup()
+
+	def _gpio_out(self, t=2):
 		GPIO.output(OUTPUT_PIN, True)
 		time.sleep(t)
 		GPIO.output(OUTPUT_PIN, False)
-		GPIO.cleanup()
 
 	def _send_text(self):
 		message = self.tw_client.messages.create(
@@ -31,7 +37,7 @@ class WaterBot:
 			body=self.config["msg_body"]
 		)
 
-		print self.config["msg_body"]
+		print datetime.now().time() + " " + self.config["msg_body"]
 
 	def _init_twilio(self):
 		return TwilioRestClient(
@@ -39,13 +45,13 @@ class WaterBot:
 			self.private_config["auth_token"]
 		)
 
-	def _get_config(self):
-		with open("config.json", "r") as f:
+	def _get_config(self, config):
+		with open(config, "r") as f:
 			config = f.read().strip()
 		return json.loads(config)
 
-	def _get_private_config(self):
-		with open("private_config.json", "r") as f:
+	def _get_private_config(self, private_config):
+		with open(private_config, "r") as f:
 			private = f.read().strip()
 		return json.loads(private)	
 
@@ -53,5 +59,13 @@ class WaterBot:
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup(output_pin, GPIO.OUT)	
 
-w = WaterBot(OUTPUT_PIN)
+
+if len(sys.argv) >= 2:
+	CONFIG = sys.argv[1]
+
+if len(sys.argv) >= 3:
+	PRIVATE_CONFIG = sys.argv[2]
+
+w = WaterBot(OUTPUT_PIN, CONFIG, PRIVATE_CONFIG)
 w.water()
+w.cleanup()
